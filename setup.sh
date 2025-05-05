7e0d7fdb32015d283201c4f6458525805f09e555
Said B. <said.benmumen@gmail.com>
2025-05-05 10:57:36 +0100

    better conf

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ path=$(pwd)
 printf "Installing dependencies...\n"
 
 sudo dnf copr enable pgdev/ghostty
-sudo dnf install -y ghostty hyprland waybar wofi grimshot neovim python3-neovim hyprpaper
+sudo dnf install -y ghostty hyprland waybar wofi grimshot neovim python3-neovim hyprpaper hyprpicker
 
 # configs symbolic links
 printf "Setting up symbolic links...\n"
