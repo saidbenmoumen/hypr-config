47f06bce2adff9dbdb6cd3af758162365bde8b70
Said B. <said.benmumen@gmail.com>
2025-05-12 06:52:08 +0000

    neo tree show hidden files

--- nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -15,6 +15,11 @@ return {
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignore = false,
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
