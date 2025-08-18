23bb9362d4ad104e4b501940104171d9265586f3
Said B. <said.benmumen@gmail.com>
2025-08-18 17:26:04 +0100

    nvim better

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      -- Extend existing formatters
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      opts.formatters_by_ft.css = { "prettier" }
+      opts.formatters_by_ft.scss = { "prettier" }
+      opts.formatters_by_ft.sass = { "prettier" }
+      opts.formatters_by_ft.less = { "prettier" }
+      opts.formatters_by_ft.astro = { "prettier" }
+
+      --
+      opts.formatters.prettier.command = "prettier"
+      opts.formatters.prettier.args = { "--stdin-filepath", "$FILENAME" }
+      opts.formatters.prettier.cwd = require("conform.util").root_file({
+        ".prettierrc",
+        "prettier.config.ts",
+        ".prettierrc.json",
+        ".prettierrc.js",
+        "package.json",
+      })
+
+      return opts
+    end,
+  },
+}
